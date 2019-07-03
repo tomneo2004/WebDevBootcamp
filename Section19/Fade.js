68faed9bbdd5ce06c8724019741b69b96fc4e6d4
Nelson <tomneo2004@gmail.com>
2019-07-03 14:41:57 +0800

    Section19

---
+++ Section19/Fade.js
@@ -0,0 +1,10 @@
+$("button").on("click", function(){
+	
+	// $("div").fadeOut(1000, function(){
+	// 	console.log("fade complete");
+	// });
+
+	//$("div").fadeToggle(1000);
+
+	$("div").slideToggle(1000);
+});
